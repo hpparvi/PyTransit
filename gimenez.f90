40c6a251463af84f4abf52cdb38fb31c67e366eb
Hannu Parviainen <hpparvi@gmail.com>
2012-11-23 12:58:55 +0000

    Major cleanup and reorganization, forget everything before this.

--- gimenez.f90
+++ gimenez.f90
@@ -38,8 +38,13 @@
 #define CHUNK_SIZE 128
 #endif
 
+#ifndef FTYPE
+#define FTYPE 8  
+#endif
+
 module gimenez
   use, intrinsic :: ISO_C_BINDING
+  use omp_lib
   implicit none
 
   integer, parameter :: FS = C_FLOAT
@@ -48,27 +53,16 @@ module gimenez
   real(8), parameter :: PI = 3.1415926525_fd
   real(8), parameter :: HALF_PI = 0.5_fd * PI
 
-  real(8), allocatable, dimension(:,:) :: a_e_nm, a_e_vl
-  real(8), allocatable, dimension(:,:,:) :: j_d, j_e
-  real(8), allocatable, dimension(:) :: tmp1d1, tmp1d2
+  real(8), dimension(100000) :: tmp1d1, tmp1d2
 
 contains
-  subroutine init(npol, nldc)
+  subroutine init_arrays(npol, nldc, a_e_nm, a_e_vl, j_d, j_e)
     implicit none
     integer, intent(in) :: npol, nldc
+    real(8), intent(out), dimension(npol, nldc+1) :: a_e_nm, a_e_vl
+    real(8), intent(out), dimension(4, npol, nldc+1) :: j_d, j_e
     integer :: i, j
     real(8) :: nu, nm1
-
-    if (allocated(a_e_nm)) deallocate(a_e_nm)
-    if (allocated(a_e_vl)) deallocate(a_e_vl)
-    if (allocated(j_d))    deallocate(j_d)
-    if (allocated(j_e))    deallocate(j_e)
-    if (allocated(tmp1d1)) deallocate(tmp1d1)
-    if (allocated(tmp1d2)) deallocate(tmp1d2)
-
-    allocate(a_e_nm(npol, nldc+1), a_e_vl(npol, nldc+1))
-    allocate(j_d(4, npol, nldc+1), j_e(4, npol, nldc+1))
-    allocate(tmp1d1(1000000), tmp1d2(1000000))
 
     do j=1,nldc+1
        nu = (real(j-1,8)+2._fd)/2._fd
@@ -92,39 +86,32 @@ contains
   contains
     subroutine j_coeff(alpha, beta, ri, res)
       implicit none
-      real(8), intent(in) :: alpha, beta, ri
-      real(8), dimension(4), intent(inout):: res
+      real(fd), intent(in) :: alpha, beta, ri
+      real(fd), dimension(4), intent(inout):: res
       res(1) = 2._fd * ri * ( ri + alpha + beta ) * ( 2._fd * ri - 2._fd + alpha + beta )
       res(2) = ( 2._fd* ri - 1._fd + alpha + beta ) * ( 2._fd * ri  + alpha + beta ) * ( 2._fd* ri - 2._fd + alpha + beta )
       res(3) = ( 2._fd* ri - 1._fd + alpha + beta ) * ( alpha + beta ) * ( alpha - beta )
       res(4) = - 2._fd * ( ri - 1._fd + alpha ) * ( ri - 1._fd + beta )  * ( 2._fd* ri + alpha + beta )
     end subroutine j_coeff
-  end subroutine init
-
-  subroutine finalize()
-    implicit none
-    if (allocated(a_e_nm)) deallocate(a_e_nm)
-    if (allocated(a_e_vl)) deallocate(a_e_vl)
-    if (allocated(j_d)) deallocate(j_d)
-    if (allocated(j_e)) deallocate(j_e)
-  end subroutine finalize
+  end subroutine init_arrays
 
 
-  subroutine eval_lerp(z, r, b, u, contamination, npol, nthreads, update, npt, nldc, res)
-    use omp_lib
+  subroutine eval_lerp(z, k, u, b, contamination, nthreads, update, npol, npt, nldc, a_e_nm, a_e_vl, j_d, j_e, res)
     implicit none
     integer, intent(in) :: npt, nldc, nthreads, npol
     logical, intent(in) :: update
     real(8), intent(in), dimension(npt) :: z
     real(8), intent(in), dimension(nldc) :: u
-    real(8), intent(in) :: r, b, contamination
+    real(8), intent(in) :: k, b, contamination
     real(8), intent(out), dimension(npt) :: res
+    real(8), intent(in), dimension(npol, nldc+1) :: a_e_nm, a_e_vl
+    real(8), intent(in), dimension(4, npol, nldc+1) :: j_d, j_e
 
     integer, parameter :: tsize = 512
     real(8), save, dimension(tsize) :: z_tbl, i_tbl
     real(8), save :: dz, idz 
     real(8) :: x
-    integer :: i, k, ntr
+    integer :: i, j, ntr
 
     logical, dimension(npt) :: mask
     real(8), dimension(npt) :: ztmp, itmp
@@ -132,23 +119,23 @@ contains
     !$ if (nthreads /= 0) call omp_set_num_threads(nthreads)
  
     if (update) then
-       dz    = (1._fd+r - b)/real(tsize-1, fd)
+       dz    = (1._fd+k - b)/real(tsize-1, fd)
        idz   = 1._fd/dz
        z_tbl = [(b + i*dz, i=0,tsize-1)]
-       call eval(z_tbl, r, u, contamination, npol, nthreads, tsize, nldc, i_tbl)
+       call eval(z_tbl, k, u, contamination, nthreads, npol, tsize, nldc, a_e_nm, a_e_vl, j_d, j_e, i_tbl)
     end if
 
-    mask        = (z > 0._fd) .and. (z < 1._fd+r)
+    mask        = (z > 0._fd) .and. (z < 1._fd+k)
     ntr         = count(mask)
     ztmp(1:ntr) = pack(z, mask)
 
-    !$omp parallel do private(i, x, k) shared(npt, z, ntr, r, b, i_tbl, idz, ztmp, itmp) default(none)
+    !$omp parallel do private(i, x, j) shared(ntr, b, i_tbl, idz, ztmp, itmp) default(none)
     do i=1,ntr
        x = (ztmp(i)-b)*idz
-       k = 1 + int(floor(x))
-       x = x - k
-       k = min(k, tsize-1)
-       itmp(i) = (1._fd-x) * i_tbl(k) + x * i_tbl(k+1)
+       j = 1 + int(floor(x))
+       x = x - j
+       j = min(j, tsize-1)
+       itmp(i) = (1._fd-x) * i_tbl(j) + x * i_tbl(j+1)
     end do 
     !$omp end parallel do
  
@@ -157,38 +144,41 @@ contains
   end subroutine eval_lerp
  
 
-  subroutine eval(z, r, u, contamination, npol, nthreads, npt, nldc, res)
-    use omp_lib
+  subroutine eval(z, k, u, contamination, nthreads, npol, npt, nldc, a_e_nm, a_e_vl, j_d, j_e, res)
     implicit none
     integer, intent(in) :: npt, nldc, nthreads, npol
     real(8), intent(in), dimension(npt) :: z
     real(8), intent(in), dimension(nldc) :: u
-    real(8), intent(in) :: r, contamination
+    real(8), intent(in) :: k, contamination
     real(8), intent(out), dimension(npt) :: res
-    
+    real(8), intent(in), dimension(npol, nldc+1) :: a_e_nm, a_e_vl
+    real(8), intent(in), dimension(4, npol, nldc+1) :: j_d, j_e
+
     logical, dimension(npt) :: mask
     integer :: npt_t
     !real(8) :: t_start, t_finish
 
     !$ if (nthreads /= 0) call omp_set_num_threads(nthreads)
     res  = 0._fd
-    mask = (z > 0._fd) .and. (z < 1._fd+r)
+    mask = (z > 0._fd) .and. (z < 1._fd+k)
     npt_t = count(mask)
 
     tmp1d1(1:npt_t) = pack(z, mask)
-    tmp1d2(1:npt_t) = gimenez_v(tmp1d1(1:npt_t), u, r, npol)
+    tmp1d2(1:npt_t) = gimenez_v(tmp1d1(1:npt_t), k, u, npol, nldc, a_e_nm, a_e_vl, j_d, j_e)
 
     res = unpack(tmp1d2(1:npt_t), mask, res)
     res = 1._fd + res*(1._fd - contamination)
   end subroutine eval
 
 
-  function gimenez_v(z, u, r, npol)
+  function gimenez_v(z, k, u, npol, nldc, a_e_nm, a_e_vl, j_d, j_e)
     implicit none
     real(8), intent(in), dimension(:) :: z
     real(8), intent(in), dimension(:) :: u    
-    real(8), intent(in) :: r
-    integer, intent(in) :: npol
+    real(8), intent(in) :: k
+    integer, intent(in) :: npol, nldc
+    real(8), intent(in), dimension(npol, nldc+1) :: a_e_nm, a_e_vl
+    real(8), intent(in), dimension(4, npol, nldc+1) :: j_d, j_e
 
     real(8), dimension(size(z)) :: gimenez_v
 
@@ -202,8 +192,9 @@ contains
     a  = 0._fd
     Cn = 1._fd
     n  = [(i, i=0,size(u))]
-    b  = r/(1._fd+r)
-    c  = z/(1._fd+r)
+    b  = k/(1._fd+k)
+    c  = z/(1._fd+k)
+
     !TODO: FIXME, I'M A HACK
     if (size(u) == 2) then
        uu = [u(1)+2._fd*u(2), -u(2)]
@@ -212,7 +203,7 @@ contains
     end if
 
     do j=0,size(u)
-       call alpha(b, c, j, npol, a(:,j+1))
+       call alpha(b, c, j, npol, nldc, a(:,j+1), a_e_nm, a_e_vl, j_d, j_e)
     end do
 
     if (size(u) > 0) then
@@ -229,11 +220,13 @@ contains
   
   !!--- Alpha ---
   !!
-  subroutine alpha(b, c, n, npol, a)
+  subroutine alpha(b, c, n, npol, nldc, a, a_e_nm, a_e_vl, j_d, j_e)
     implicit none
     real(8), intent(in), dimension(:) :: b, c
-    integer, intent(in) :: n, npol
+    integer, intent(in) :: n, npol, nldc
     real(8), dimension(size(c)), intent(out) :: a
+    real(8), intent(in), dimension(npol, nldc+1) :: a_e_nm, a_e_vl
+    real(8), intent(in), dimension(4, npol, nldc+1) :: j_d, j_e
 
     real(8), dimension(size(c)) :: norm, sm
     real(8), dimension(1, npol) :: e
@@ -292,7 +285,6 @@ end subroutine alpha
   !! multiple x at the same time.
   !!
   subroutine jacobi(npol, alpha, beta, x, i_ld, j_c, cx)
-    use omp_lib
     implicit none
     integer, intent(in) :: npol, i_ld
     real(8), intent(in) :: alpha, beta
