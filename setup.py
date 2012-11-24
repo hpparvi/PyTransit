0ced7b3244d8543445a36a7054fd9081f539067c
Hannu Parviainen <hpparvi@gmail.com>
2012-11-24 20:43:53 +0000

    Adding a basic setup script.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from numpy.distutils.core import setup, Extension
+from numpy.distutils.misc_util import Configuration
+
+import distutils.sysconfig as ds
+
+import sys
+sys.argv.extend(['config_fc','--fcompiler=gnu95', '--opt="-Ofast -ffast-math"', '--f90flags="-cpp -fopenmp -march=native"'])
+
+conf = Configuration()
+
+setup(name='PyTransit',
+      version='0.5',
+      description='Tools for exoplanet transit light curve analysis.',
+      author='Hannu Parviainen',
+      author_email='hpparvi@gmail.com',
+      url='',
+      extra_options = ['-fopenmp'],
+      py_modules=['gimenez'],
+      ext_modules=[Extension('gimenez_f',                    ['gimenez.f90'], libraries=['gomp','m'], define_macros=[('DCHUNK_SIZE',128)], extra_f90_compile_args=['-cpp'])]
+     )
