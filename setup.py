b5c09972f806a1d98893deac4111e63228e90616
Hannu Parviainen <hpparvi@gmail.com>
2013-01-09 14:31:21 +0000

    Cleanup

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@ from numpy.distutils.core import setup, Extension
 from numpy.distutils.misc_util import Configuration
 import distutils.sysconfig as ds
 
-#import sys
-#sys.argv.extend(['config_fc','--fcompiler=gnu95', '--opt="-Ofast -ffast-math"', '--f90flags="-cpp -fopenmp -march=native"'])
-
 setup(name='PyTransit',
       version='0.5',
       description='Tools for exoplanet transit light curve analysis.',
@@ -14,6 +11,6 @@ setup(name='PyTransit',
       extra_options = ['-fopenmp'],
       package_dir={'pytransit':'src'},
       packages=['pytransit'],
-      ext_modules=[Extension('pytransit.gimenez_f', ['src/gimenez.f90'], libraries=['gomp','m'], define_macros=[('DCHUNK_SIZE',128)], extra_f90_compile_args=['-cpp']),
-                   Extension('pytransit.orbits_f',  ['src/orbits.f90','src/orbits.pyf'], libraries=['gomp','m'], extra_f90_compile_args=['-cpp'])]
+      ext_modules=[Extension('pytransit.gimenez_f', ['src/gimenez.f90'], libraries=['gomp','m'], define_macros=[('DCHUNK_SIZE',128)]),
+                   Extension('pytransit.orbits_f',  ['src/orbits.f90','src/orbits.pyf'], libraries=['gomp','m'])]
      )
