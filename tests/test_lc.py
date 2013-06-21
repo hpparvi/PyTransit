864e39ecd34ab9957fb8104b189329f131e0b4fd
Hannu Parviainen <hpparvi@gmail.com>
2013-06-21 20:28:26 +0000

    Modified test scripts.

--- tests/test_lc.py
+++ tests/test_lc.py
@@ -10,13 +10,15 @@ ni = 150
 
 ## Test the calculation of projected distances
 ## ===========================================
-m = Gimenez()
+m = Gimenez(npol=200)
 t = np.linspace(0.94,1.06,5000)
 
-t0, p, a, i = 1, 2, 7, 0.49*np.pi
+t0, p, a, i = 1, 2, 7, 0.5*np.pi
 
 z = of.z_circular(t, t0, p, a, i, 0)
-u = np.array([[0.2,0.1],[0.4,0.2],[0.6,0.3]]).T
+u = [[uu,vv] for uu,vv in zip(np.linspace(0,0.8,20), np.linspace(0,0.4,20))]
+#u = [[0.2,0.1],[0.4,0.2],[0.6,0.3]]
+#u = [0.2,0.1, 0.4,0.2, 0.6,0.3]
 
 
 lc = m(z, 0.1, u)
--- tests/test_speed.py
+++ tests/test_speed.py
@@ -6,12 +6,13 @@ from mpi4py.MPI import Wtime
 from pytransit.gimenez import Gimenez
 
 ni = 250
-
 z  = np.tile(np.linspace(1e-7,1.2,1000),20)
-us = [np.array([[0.2,0.1]]).T,
-      np.array([[0.2,0.1],[0.4,0.2]]).T,
-      np.array([[0.2,0.1],[0.4,0.2],[0.6,0.3]]).T,
-      np.array([[0.2,0.1],[0.4,0.2],[0.6,0.3], [0.0, 0.4]]).T]
+us = [ [0.2,0.1],
+      [[0.2,0.1],[0.4,0.2]],
+      [ 0.2,0.1 , 0.4,0.2 , 0.6,0.3],
+      [[0.2,0.1],[0.4,0.2],[0.6,0.3], [0.0, 0.4]],
+       [[uu,vv] for uu,vv in zip(np.linspace(0,0.8,20), np.linspace(0,0.4,20))]
+]
 
 for u in us:
     for npol in [200, 100, 50, 20]:
@@ -19,5 +20,5 @@ for u in us:
         m = Gimenez(npol=npol)
         for i in range(ni):
             m(z, 0.1, u)
-        print "npb {:3d}:  npol {:3d}: {:8.6f} s/lc".format(u.shape[1], npol, (Wtime()-tstart)/(ni))
+        print "npb {:3d}:  npol {:3d}: {:8.6f} s/lc".format(len(u), npol, (Wtime()-tstart)/(ni))
     print
