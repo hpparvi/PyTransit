1aa15a895e5b476c0f56b10fb1e626ae48046218
Hannu Parviainen <hpparvi@gmail.com>
2013-06-21 19:44:26 +0000

    Modified the speed test script.

--- tests/test_speed.py
+++ tests/test_speed.py
@@ -8,9 +8,9 @@ from pytransit.gimenez import Gimenez
 ni = 150
 
 z  = np.tile(np.linspace(1e-7,1.2,1000),20)
-u  = np.array([0.4,0.1])
+u  = np.array([[0.2,0.1],[0.4,0.2],[0.6,0.3]]).T
 
-for npol in [500, 200, 100, 50, 20]:
+for npol in [200, 100, 50, 20]:
     tstart = Wtime()
     m = Gimenez(npol=npol)
     for i in range(ni):
