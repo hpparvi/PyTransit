194c9702f0f05576955de693ba45dd309086e31f
Hannu Parviainen <hpparvi@gmail.com>
2013-06-21 19:50:35 +0000

    Modified the speed test script.

--- tests/test_speed.py
+++ tests/test_speed.py
@@ -5,14 +5,19 @@ import matplotlib.pyplot as pl
 from mpi4py.MPI import Wtime
 from pytransit.gimenez import Gimenez
 
-ni = 150
+ni = 250
 
 z  = np.tile(np.linspace(1e-7,1.2,1000),20)
-u  = np.array([[0.2,0.1],[0.4,0.2],[0.6,0.3]]).T
+us = [np.array([[0.2,0.1]]).T,
+      np.array([[0.2,0.1],[0.4,0.2]]).T,
+      np.array([[0.2,0.1],[0.4,0.2],[0.6,0.3]]).T,
+      np.array([[0.2,0.1],[0.4,0.2],[0.6,0.3], [0.0, 0.4]]).T]
 
-for npol in [200, 100, 50, 20]:
-    tstart = Wtime()
-    m = Gimenez(npol=npol)
-    for i in range(ni):
-        m(z, 0.1, u)
-    print "npol {:3d}: {:8.6f} s/lc".format(npol, (Wtime()-tstart)/ni)
+for u in us:
+    for npol in [200, 100, 50, 20]:
+        tstart = Wtime()
+        m = Gimenez(npol=npol)
+        for i in range(ni):
+            m(z, 0.1, u)
+        print "npb {:3d}:  npol {:3d}: {:8.6f} s/lc".format(u.shape[1], npol, (Wtime()-tstart)/(ni))
+    print
