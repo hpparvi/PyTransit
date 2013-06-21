d63c15d20359111cb74fdb5f8aea63022f988f60
Hannu Parviainen <hpparvi@gmail.com>
2013-06-21 20:31:36 +0000

    Modified test scripts.

--- tests/test_lc.py
+++ tests/test_lc.py
@@ -10,19 +10,18 @@ ni = 150
 
 ## Test the calculation of projected distances
 ## ===========================================
-m = Gimenez(npol=200)
+m1 = Gimenez(npol=200, lerp=False)
+m2 = Gimenez(npol=200, lerp=True)
+
 t = np.linspace(0.94,1.06,5000)
 
 t0, p, a, i = 1, 2, 7, 0.5*np.pi
 
 z = of.z_circular(t, t0, p, a, i, 0)
 u = [[uu,vv] for uu,vv in zip(np.linspace(0,0.8,20), np.linspace(0,0.4,20))]
-#u = [[0.2,0.1],[0.4,0.2],[0.6,0.3]]
-#u = [0.2,0.1, 0.4,0.2, 0.6,0.3]
 
+pl.plot(m1(z, 0.1, u))
+pl.plot(m2(z, 0.1, u))
 
-lc = m(z, 0.1, u)
-
-pl.plot(lc)
 pl.ylim(0.985, 1.001)
 pl.show()
