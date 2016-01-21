2ab61a36882e2e4b7c1675f7acde7262046b70ab
Hannu Parviainen <hpparvi@gmail.com>
2016-01-21 12:50:20 +0000

    Improved the z calculation tests.

--- tests/unit_tests/test_z.py
+++ tests/unit_tests/test_z.py
@@ -2,14 +2,12 @@ from __future__ import division
 import unittest
 import numpy as np
 import numpy.testing as npt
+from math import pi
 
 from pytransit.orbits_f import orbits as of
 
 class TestCircularZ(unittest.TestCase):
-    """Test the routines to calculate the normalized projected distance (z) assuming e=0 and w=0.
-
-    NOTE:
-      The normalized projected distance should be negative during an eclipse.
+    """Test the routines to calculate the normalized projected distance (z) assuming zero eccentricity.
     """
     def setUp(self):
         self.t  = [0, 0.125, 0.25, 0.375, 0.5, 0.75]
@@ -18,30 +16,36 @@ class TestCircularZ(unittest.TestCase):
         self.a  = 5
         self.i  = 0.5*np.pi
         self.zref = np.array([0.0 , 3.535533, 5.0 , -3.535533, -0.0, -5.0])
-
+        self.ws = [0, 0.5*pi, pi, 1.5*pi, 2*pi, 0.012, 0.24, -0.53]
+        
     def test_circular(self):
-        z = of.z_circular(self.t, self.t0, self.p, self.a, self.i, nthreads=1)
+        z = of.z_circular(self.t, self.t0, self.p, self.a, self.i, nth=1)
         npt.assert_array_almost_equal(z, self.zref)
 
     def test_ip(self):
-        z = of.z_eccentric_ip(self.t, self.t0, self.p, self.a, self.i, 0, 0, nthreads=1, update=True)
-        npt.assert_array_almost_equal(z, self.zref, decimal=4)
-
+        for w in self.ws:
+            z = of.z_eccentric_ip(self.t, self.t0, self.p, self.a, self.i, 0, w, nth=1, update=True)
+            npt.assert_array_almost_equal(z, self.zref, decimal=4)
+                
     def test_iter(self):
-        z = of.z_eccentric_iter(self.t, self.t0, self.p, self.a, self.i, 0, 0, nthreads=1)
-        npt.assert_array_almost_equal(z, self.zref)
-
+        for w in self.ws:
+            z = of.z_eccentric_iter(self.t, self.t0, self.p, self.a, self.i, 0, w, nth=1)
+            npt.assert_array_almost_equal(z, self.zref)
+ 
     def test_newton(self):
-        z = of.z_eccentric_newton(self.t, self.t0, self.p, self.a, self.i, 0, 0, nthreads=1)
-        npt.assert_array_almost_equal(z, self.zref)
+        for w in self.ws:
+            z = of.z_eccentric_newton(self.t, self.t0, self.p, self.a, self.i, 0, w, nth=1)
+            npt.assert_array_almost_equal(z, self.zref)
 
     def test_ps3(self):
-        z = of.z_eccentric_ps3(self.t, self.t0, self.p, self.a, self.i, 0, 0, nthreads=1)
-        npt.assert_array_almost_equal(z, self.zref)
+        for w in self.ws:
+            z = of.z_eccentric_ps3(self.t, self.t0, self.p, self.a, self.i, 0, w, nth=1)
+            npt.assert_array_almost_equal(z, self.zref)
 
     def test_ps5(self):
-        z = of.z_eccentric_ps5(self.t, self.t0, self.p, self.a, self.i, 0, 0, nthreads=1)
-        npt.assert_array_almost_equal(z, self.zref)
+        for w in self.ws:
+            z = of.z_eccentric_ps5(self.t, self.t0, self.p, self.a, self.i, 0, w, nth=1)
+            npt.assert_array_almost_equal(z, self.zref)
 
 if __name__ == '__main__':
     unittest.main()
