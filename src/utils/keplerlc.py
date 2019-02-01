ffad068d2b0313cd0332096f706fded2de3e628b
Hannu Parviainen <hannu@iac.es>
2019-02-01 19:14:47 +0000

    Updated copyrights.

--- src/utils/keplerlc.py
+++ src/utils/keplerlc.py
@@ -1,8 +1,20 @@
-import numpy as np
-from scipy.ndimage import label
-import matplotlib.pyplot as pl
+#  PyTransit: fast and easy exoplanet transit modelling in Python.
+#  Copyright (C) 2010-2019  Hannu Parviainen
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from .utils.misc import fold
+import numpy as np
 
 def fold_orbit_and_phase(time, origo, period, shift):
     phase  = ((time - origo)/period + shift)
