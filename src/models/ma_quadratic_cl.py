074b5dc721275aabf269d2683836e1db54b7ab6c
Hannu Parviainen <hannu@iac.es>
2019-04-23 22:07:51 +0100

    Quadratic OpenCL model bugfix and example notebook.

--- src/models/ma_quadratic_cl.py
+++ src/models/ma_quadratic_cl.py
@@ -38,41 +38,23 @@ import numpy as np
 import pyopencl as cl
 from os.path import dirname, join
 
+import warnings
+from pyopencl import CompilerWarning
+
 from numpy import array, uint32, float32, int32, asarray, zeros, ones, unique
 
 from .numba.ma_quadratic_nb import calculate_interpolation_tables
 from .transitmodel import TransitModel
 
+warnings.filterwarnings('ignore', category=CompilerWarning)
 
 class QuadraticModelCL(TransitModel):
     """
     Exoplanet transit light curve model by Mandel and Agol (2001).
-
-    :param nldc: (optional)
-        Number of limb darkening coefficients (1 = linear limb darkening, 2 = quadratic)
-
-    :param supersampling: (optional)
-        Number of subsamples to calculate for each light curve point
-
-    :param exptime: (optional)
-        Integration time for a single exposure, used in supersampling
-
-
-    Examples
-    --------
-
-    Basic case::
-
-      m = MandelAgolCL() # Initialize the model, use quadratic limb darkening law and all available cores
-      I = m(z,k,u)       # Evaluate the model for projected distance z, radius ratio k, and limb darkening coefficients u
-      
     """
 
-    def __init__(self, npb: int = 1, eccentric: bool = False, constant_k: bool = True,
-                 contamination: bool = False, optimize: bool = False, eclipse: bool = False, supersampling: int = 1,
-                 exptime: float = 0.020433598, klims: tuple = (0.05, 0.25), nk: int = 256, nz: int = 256,
-                 cl_ctx=None, cl_queue=None):
-        super().__init__(npb, eccentric, constant_k, contamination=contamination, optimize=optimize, eclipse=eclipse)
+    def __init__(self, method: str = 'pars', is_secondary: bool = False, klims: tuple = (0.05, 0.25), nk: int = 256, nz: int = 256, cl_ctx=None, cl_queue=None) -> None:
+        super().__init__(method, is_secondary)
 
         self.ctx = cl_ctx or cl.create_some_context()
         self.queue = cl_queue or cl.CommandQueue(self.ctx)
@@ -89,9 +71,6 @@ class QuadraticModelCL(TransitModel):
         self.k0, self.k1 = map(float32, self.kt[[0,-1]])
         self.dk = float32(self.kt[1]-self.kt[0])
         self.dz = float32(self.zt[1]-self.zt[0])
-        self.nss = int32(supersampling)
-        self.etime = float32(exptime)
-
         self.pv = array([])
 
         mf = cl.mem_flags
@@ -120,7 +99,7 @@ class QuadraticModelCL(TransitModel):
 
         self._time_id = None   # Time array ID
 
-        self.prg = cl.Program(self.ctx, open(join(dirname(__file__),'opencl','ma_cuadratic.cl'),'r').read()).build()
+        self.prg = cl.Program(self.ctx, open(join(dirname(__file__),'opencl','ma_quadratic.cl'),'r').read()).build()
 
 
     def set_data(self, time, lcids=None, pbids=None, nsamples=None, exptimes=None):
@@ -138,7 +117,7 @@ class QuadraticModelCL(TransitModel):
         self.nptb = time.size
 
         self.time = asarray(time, dtype='float32')
-        self.lcids = zeros(t.size, 'uint32') if lcids is None else asarray(lcids, dtype='uint32')
+        self.lcids = zeros(time.size, 'uint32') if lcids is None else asarray(lcids, dtype='uint32')
         self.pbids = zeros(self.nlc, 'uint32') if pbids is None else asarray(pbids, dtype='uint32')
         self.nsamples = ones(self.nlc, 'uint32') if nsamples is None else asarray(nsamples, dtype='uint32')
         self.exptimes = ones(self.nlc, 'float32') if exptimes is None else asarray(exptimes, dtype='float32')
@@ -151,51 +130,9 @@ class QuadraticModelCL(TransitModel):
 
 
     def evaluate_ps(self, k, ldc, t0, p, a, i, e=0., w=0., copy=True):
-        ldc = np.array(ldc, float32, order='C').T
-
-        # Release and reinitialise the GPU buffers if the sizes of the time or
-        # limb darkening coefficient arrays change.
-        if (ldc.size != self.u.size):
-            if self._b_u is not None:
-                self._b_f.release()
-                self._b_u.release()
-                self._b_p.release()
-
-            self.npb = 1 if ldc.ndim == 1 else ldc.shape[1]
-            self.nptb = t.size
-
-            self.u = np.zeros((2,self.npb), float32)
-            self.f = np.zeros((self.nptb, self.npb), float32)
-            self.pv = np.zeros(7, float32)
-
-            mf = cl.mem_flags
-            self._b_time = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=t)
-            self._b_f = cl.Buffer(self.ctx, mf.WRITE_ONLY, t.nbytes)
-            self._b_u = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.u)
-            self._b_p = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.pv)
-
-        # Copy the time array to the GPU if it has been changed
-        if id(t) != self._time_id:
-            cl.enqueue_copy(self.queue, self._b_time, t)
-            self._time_id = id(t)
-
-        # Copy the limb darkening coefficient array to the GPU
-        cl.enqueue_copy(self.queue, self._b_u, ldc)
-
-        # Copy the parameter vector to the GPU
-        self.pv[:] = np.array([k, t0, p, a, i, e, w], dtype=float32)
-        cl.enqueue_copy(self.queue, self._b_p, self.pv)
-
-        self.prg.ma_eccentric(self.queue, t.shape, None, self._b_time, self._b_p, self._b_u,
-                              self._b_ed, self._b_le, self._b_ld, self.nss, self.etime,
-                              self.k0, self.k1, self.nk, self.nz, self.dk, self.dz, self._b_f)
-
-        if copy:
-            cl.enqueue_copy(self.queue, self.f, self._b_f)
-            return self.f
-        else:
-            return None
-
+        ldc = asarray(ldc, float32)
+        pvp = array([[k, t0, p, a, i, e, w]], float32)
+        return self.evaluate_pv(pvp, ldc, copy)
 
     def evaluate_pv(self, pvp, ldc, copy=True):
         ldc = asarray(ldc, float32)
