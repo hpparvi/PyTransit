ccd30849caa6733a3c2c72a8e9988249693f7959
Hannu Parviainen <hannu@iac.es>
2020-04-12 16:25:13 +0100

    Improved the radial velocity model.

--- pytransit/lpf/rvlpf.py
+++ pytransit/lpf/rvlpf.py
@@ -26,15 +26,17 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from typing import Iterable, Optional
 
 from matplotlib.pyplot import subplots
 from numba import njit
 from numpy import zeros, log, pi, inf, atleast_2d, zeros_like, arange, arctan2, cos, squeeze, median, linspace, \
-    percentile, argsort, sum
+    percentile, argsort, sum, concatenate, full
 from numpy.random.mtrand import permutation
 
+from pytransit.lpf.logposteriorfunction import LogPosteriorFunction
 from pytransit.utils.misc import fold
-from pytransit.param import GParameter, UniformPrior as UP, NormalPrior as NP
+from pytransit.param import GParameter, UniformPrior as UP, NormalPrior as NP, ParameterSet
 from pytransit.orbits.orbits_py import ta_newton_v
 
 
@@ -51,54 +53,115 @@ class RVModel:
     """Radial velocity mixin class to allow for customised joint modelling of transits and RVs.
     """
 
-    def __init__(self):
-        self.rv_times = None
-        self.rv_values = None
-        self.rv_errors = None
+    def __init__(self, nplanets: int, times, rvs: Iterable, rves: Iterable, rvis: Iterable,
+                 tref: Optional[float] = None, lpf: Optional[LogPosteriorFunction] = None):
+        self.lpf = lpf
 
-        self._read_rv_data()
+        if lpf is None:
+            if hasattr(self, 'nplanets'):
+                assert self.nplanets == nplanets
+            else:
+                self.nplanets = nplanets
 
-        self.ps.thaw()
+            if not hasattr(self, 'ps') or self.ps is None:
+                self._init_parameters()
+
+        # Reference time setup
+        # --------------------
+        if lpf is None:
+            self.tref = tref if tref is not None else 0.0
+        else:
+            assert tref is None
+            self.tref = lpf.tref if hasattr(lpf, 'tref') else 0.0
+
+        # Define the main data lists and arrays
+        # -------------------------------------
+        self.times = None  # Mid-measurement times
+        self.rvs = None  # RV values
+        self.rves = None  # RV errors
+        self.rvis = None  # RV instrument indices
+
+        self._timea = None
+        self._rva = None
+        self._rvea = None
+        self._rv_ids = None
+
+        self.setup_data(times, rvs, rves, rvis)
         self._init_p_rv()
-        self.ps.freeze()
 
-    def _read_rv_data(self):
-        raise NotImplementedError
+    def setup_data(self, times: Iterable, rvs: Iterable, rves: Iterable, rvis: Iterable):
+        self.times = times
+        self.rvs = rvs
+        self.rves = rves
+        self.rvis = rvis
+
+        self._timea = concatenate(times) - self.tref
+        self._rva = concatenate(rvs)
+        self._rvea = concatenate(rves)
+        self._rv_ids = concatenate([full(c.size, i) for i, c in enumerate(self.times)])
+
+    def _init_parameters(self):
+        self.ps = ps = ParameterSet([])
+        pp = []
+        for i in range(1, self.nplanets + 1):
+            pp.extend([
+                GParameter(f'tc_{i}', f'zero_epoch_{i}', 'd', NP(0.0, 0.1), (-inf, inf)),
+                GParameter(f'p_{i}', f'period_{i}', 'd', NP(1.0, 1e-5), (0, inf)),
+                GParameter(f'secw_{i}', f'sqrt(e)_cos(w)_{i}', 'rad', UP(-1.0, 1.0), (-1, 1)),
+                GParameter(f'sesw_{i}', f'sqrt(e)_sin(w)_{i}', '', UP(-1.0, 1.0), (-1, 1)),
+            ])
+        ps.add_global_block('planets', pp)
+        self._start_pl = ps.blocks[-1].start
+        self._sl_pl = ps.blocks[-1].slice
 
     def _init_p_rv(self):
+        self.ps.thaw()
         ps = self.ps
-        prv = [GParameter('rv_sys', 'systemic_velocity', 'm/s', NP(0.0, 0.1), (-inf, inf))]
+        prv = [GParameter(f'rv_shift_{self.rvis[i]}', 'systemic_velocity', 'm/s', NP(0.0, 0.1), (-inf, inf)) for i in
+               range(len(self.times))]
+        ps.add_global_block('rv_shifts', prv)
+        self._start_rvs = ps.blocks[-1].start
+        self._sl_rvs = ps.blocks[-1].slice
+
+        prv = []
         for i in range(1, self.nplanets + 1):
             prv.append(GParameter(f'rv_k_{i}', f'rv_semiamplitude_{i}', 'm/s', UP(0.0, 1.0), (0, inf)))
-        ps.add_global_block('rv', prv)
-        self._start_rv = ps.blocks[-1].start
-        self._sl_rv = ps.blocks[-1].slice
+        ps.add_global_block('rv_semiamplitudes', prv)
+        self._start_rvk = ps.blocks[-1].start
+        self._sl_rvk = ps.blocks[-1].slice
+        self.ps.freeze()
+
+        pnames = "rv_k_{} tc_{} p_{} secw_{} sesw_{}".split()
+        self.pids = zeros((self.nplanets, 5), 'int')
+        for ipl in range(self.nplanets):
+            for ip, p in enumerate(pnames):
+                name = p.format(ipl + 1)
+                self.pids[ipl, ip] = self.ps.names.index(name)
+
+    def rv_shifts(self, pvp):
+        pvp = atleast_2d(pvp)
+        return squeeze(pvp[:, self._sl_rvs][:, self._rv_ids])
 
     def rv_model(self, pvp, times=None, planets=None, add_sv=True):
-        times = self.rv_times if times is None else times
+        times = self._timea if times is None else times
         pvp = atleast_2d(pvp)
-        pvp_orbits = pvp[:, self._sl_pl]
-        pvp_rvs = pvp[:, self._sl_rv]
-
         rvs = zeros((pvp.shape[0], times.size))
-        tas = zeros_like(rvs)
 
         planets = planets if planets is not None else arange(self.nplanets)
-
         for ipl in planets:
-            pv = pvp_orbits[:, ipl * 6:(1 + ipl) * 6]
-            tc, p = pv[:, 1], pv[:, 2]
-            e = pv[:, 4] ** 2 + pv[:, 5] ** 2
-            w = arctan2(pv[:, 5], pv[:, 4])
+            pv = pvp[:, self.pids[ipl]]
+            tc, p = pv[:, 1] - self.tref, pv[:, 2]
+            e = pv[:, 3] ** 2 + pv[:, 4] ** 2
+            w = arctan2(pv[:, 4], pv[:, 3])
             for ipv in range(pv.shape[0]):
-                tas[ipv, :] = ta_newton_v(times, t0=tc[ipv], p=p[ipv], e=e[ipv], w=w[ipv])
-                rvs[ipv] += pvp_rvs[ipv, 1 + ipl] * (cos(w[ipv] + tas[ipv]) + e[ipv] * cos(w[ipv]))
+                ta = ta_newton_v(times, t0=tc[ipv], p=p[ipv], e=e[ipv], w=w[ipv])
+                rvs[ipv] += pv[ipv, 0] * (cos(w[ipv] + ta) + e[ipv] * cos(w[ipv]))
         if add_sv:
-            rvs += pvp_rvs[:, 0:1]
+            rvs += self.rv_shifts(pvp)
         return squeeze(rvs)
 
-    def lnlikelihood_rv(self, pv):
-        return lnlike_normal(self.rv_values, self.rv_model(pv), self.rv_errors)
+    def lnlikelihood(self, pv):
+        return lnlike_normal(self._rva, self.rv_model(pv), self._rvea)
 
     def plot_rv_vs_time(self, method='de', pv=None, nsamples: int = 200, ntimes: int = 500, axs=None):
 
@@ -127,14 +190,13 @@ class RVModel:
                 pvp = permutation(pv)[:nsamples, :]
                 pv = median(pvp, 0)
 
-        rv_sys = pv[self._start_rv]
-        rv_time = linspace(self.rv_times.min() - 1, self.rv_times.max() + 1, num=ntimes)
+        rv_time = linspace(self._timea.min() - 1, self._timea.max() + 1, num=ntimes)
 
         if pvp is None:
-            rv_model = self.rv_model(pv, rv_time) - rv_sys
+            rv_model = self.rv_model(pv, rv_time, add_sv=False)
             rv_model_limits = None
         else:
-            rv_percentiles = percentile(self.rv_model(pvp, rv_time), [50, 16, 84, 2.5, 97.5], 0) - rv_sys
+            rv_percentiles = percentile(self.rv_model(pvp, rv_time, add_sv=False), [50, 16, 84, 2.5, 97.5], 0)
             rv_model = rv_percentiles[0]
             rv_model_limits = rv_percentiles[1:]
 
@@ -143,8 +205,8 @@ class RVModel:
             axs[0].fill_between(rv_time, rv_model_limits[0], rv_model_limits[1], facecolor='darkblue', alpha=0.5)
 
         axs[0].plot(rv_time, rv_model, 'k', lw=1)
-        axs[0].errorbar(self.rv_times, self.rv_values - rv_sys, self.rv_errors, fmt='ok')
-        axs[1].errorbar(self.rv_times, self.rv_values - self.rv_model(pv), self.rv_errors, fmt='ok')
+        axs[0].errorbar(self._timea, self._rva - self.rv_shifts(pv), self._rvea, fmt='ok')
+        axs[1].errorbar(self._timea, self._rva - self.rv_model(pv), self._rvea, fmt='ok')
 
         if fig is not None:
             fig.tight_layout()
@@ -177,18 +239,17 @@ class RVModel:
                 pvp = permutation(pv)[:nsamples, :]
                 pv = median(pvp, 0)
 
-        rv_sys = pv[self._start_rv]
-        rv_time = linspace(self.rv_times.min() - 1, self.rv_times.max() + 1, num=ntimes)
+        rv_time = linspace(self._timea.min() - 1, self._timea.max() + 1, num=ntimes)
 
         all_planets = set(range(self.nplanets))
         other_planets = all_planets.difference([planet])
 
         if pvp is None:
-            rv_model = self.rv_model(pv, rv_time, [planet]) - rv_sys
+            rv_model = self.rv_model(pv, rv_time, [planet], add_sv=False)
             rv_others = self.rv_model(pv, planets=other_planets, add_sv=False)
             rv_model_limits = None
         else:
-            rv_percentiles = percentile(self.rv_model(pvp, rv_time, [planet]), [50, 16, 84, 2.5, 97.5], 0) - rv_sys
+            rv_percentiles = percentile(self.rv_model(pvp, rv_time, [planet], add_sv=False), [50, 16, 84, 2.5, 97.5], 0)
             rv_model = rv_percentiles[0]
             rv_model_limits = rv_percentiles[1:]
             rv_others = median(self.rv_model(pvp, planets=other_planets, add_sv=False), 0)
@@ -196,7 +257,7 @@ class RVModel:
         period = pv[self.ps.names.index(f'p_{planet + 1}')]
         tc = pv[self.ps.names.index(f'tc_{planet + 1}')]
 
-        phase = (fold(self.rv_times, period, tc, 0.5) - 0.5) * period
+        phase = (fold(self._timea, period, tc, 0.5) - 0.5) * period
         phase_model = (fold(rv_time, period, tc, 0.5) - 0.5) * period
         msids = argsort(phase_model)
 
@@ -207,9 +268,9 @@ class RVModel:
             axs[0].fill_between(phase_model[msids], rv_model_limits[0, msids], rv_model_limits[1, msids],
                                 facecolor='darkblue',
                                 alpha=0.25)
-        axs[0].errorbar(phase, self.rv_values - rv_others - rv_sys, self.rv_errors, fmt='ok')
+        axs[0].errorbar(phase, self._rva - rv_others - self.rv_shifts(pv), self._rvea, fmt='ok')
         axs[0].plot(phase_model[msids], rv_model[msids], 'k')
-        axs[1].errorbar(phase, self.rv_values - self.rv_model(pv), self.rv_errors, fmt='ok')
+        axs[1].errorbar(phase, self._rva - self.rv_model(pv), self._rvea, fmt='ok')
 
         if fig is not None:
             fig.tight_layout()
