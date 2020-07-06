b6d91401ff48a666c18e2cc9231bb66d7d1bfcf7
Hannu Parviainen <hannu@iac.es>
2020-07-06 23:27:50 +0200

    Improved the LDTkLDModel.

--- pytransit/models/ldtkldm.py
+++ pytransit/models/ldtkldm.py
@@ -64,15 +64,24 @@ def eval_ldm_frozen(emu, mu, z, mldps, npv):
 
 
 class LDModel:
-    def __init__(self):
-        self._int_z = linspace(0, 1, 200)
+    def __init__(self, niz: int = 200):
+        self._int_z = linspace(0, 1, niz)
         self._int_mu = sqrt(1 - self._int_z ** 2)
 
-    def __call__(self, mu: ndarray, x: ndarray):
+    def __call__(self, mu: ndarray, x: ndarray) -> Tuple[ndarray, ndarray]:
+        return self._evaluate(mu, x), self._integrate(x)
+
+    def _evaluate(self, mu: ndarray, x:ndarray) -> ndarray:
         raise NotImplementedError
 
-    def integrate(self, x: ndarray) -> float:
-        return 2 * pi * trapz(self._int_z * self(self._int_mu, x), self._int_z)
+    def _integrate(self, x: ndarray) -> ndarray:
+        npv = x.shape[0]
+        npb = x.shape[1]
+        ldi = zeros((npv, npb))
+        for ipv in range(npv):
+            for ipb in range(npb):
+                ldi[ipv,ipb] = 2. * pi * trapz(self._int_z * self(self._int_mu, x), self._int_z)
+        return ldi
 
 
 class LDTkLDModel(LDModel):
@@ -99,5 +108,8 @@ class LDTkLDModel(LDModel):
         else:
             return eval_ldm(mu, self.mu, self.z, self.profiles, npv, self.nsamples)
 
-    def integrate(self, x: ndarray) -> float:
+    def _evaluate(self, mu: ndarray, x:ndarray) -> ndarray:
+        raise NotImplementedError
+
+    def _integrate(self, x: ndarray) -> float:
         raise NotImplementedError
