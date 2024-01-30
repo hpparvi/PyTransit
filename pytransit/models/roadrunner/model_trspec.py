c19132e78ce88ae627bf51000c397f4ae1a4eed7
Hannu Parviainen <hpparvi@gmail.com>
2024-01-30 16:50:13 +0000

    Updated the transmission spectrum RRModel to always use parameter vector arrays.

--- pytransit/models/roadrunner/model_trspec.py
+++ pytransit/models/roadrunner/model_trspec.py
@@ -10,65 +10,77 @@ from .common import circle_circle_intersection_area_kite as ccia
 
 
 @njit(parallel=False, fastmath=False)
-def rr_simple_serial(times: ndarray, k: ndarray, t0: float, p: float, a: float, i: float, e: float, w: float,
-                     nsamples: int, exptimes: float, ldp: ndarray, istar: ndarray,
+def rr_simple_serial(times: ndarray, k: ndarray, t0: ndarray, p: ndarray, a: ndarray, i: ndarray, e: ndarray,
+                     w: ndarray,
+                     nsamples: ndarray, exptimes: ndarray, ldp: ndarray, istar: ndarray,
                      weights: ndarray, dk: float, kmin: float, kmax: float, dg: float, z_edges: ndarray):
+    if k.ndim != 2:
+        raise ValueError(" The radius ratios must be given as a 2D array with shape (npv, npb)")
+
+    if ldp.ndim != 3:
+        raise ValueError("The limb darkening profiles must be given as a 3D array with shape (npv, npb, nmu)")
+
+    if k.size != ldp.shape[1]:
+        raise ValueError(
+            "The transmission spectrum transit model requires that the number or radius ratios and the number of passbands match.")
+
     npt = times.size
-    npb = ldp.shape[1]
+    npv = k.shape[0]
+    npb = k.shape[1]
     ng = weights.shape[1]
 
-    if k.size != npb:
-        raise ValueError("The number or radius ratios must match the number of passbands.")
+    flux = zeros((npv, npb, npt))  # Model flux
+    ldm = zeros((npb, ng))         # Limb darkening means
+    xyc = zeros((2, 5))            # Taylor series coefficients for the (x, y) position
 
-    if isnan(a) or (a <= 1.0) or (e < 0.0):
-        return full((npb, npt), nan)
+    for ipv in range(npv):
+        if isnan(a[ipv]) or (a[ipv] <= 1.0) or (e[ipv] < 0.0):
+            flux[ipv, :, :] = nan
+            continue
 
-    kmean = mean(k)
-    afac = k ** 2 / kmean ** 2
+        kmean = mean(k[ipv])
+        afac = k[ipv] ** 2 / kmean ** 2
 
-    ldm = zeros((npb, ng))  # Limb darkening means
-    xyc = zeros((2, 5))  # Taylor series coefficients for the (x, y) position
-
-    # ----------------------------------#
-    # Calculate the limb darkening mean #
-    # ----------------------------------#
-    if kmin <= kmean <= kmax:
-        ik = int(floor((kmean - kmin) / dk))
-        ak = (kmean - kmin - ik * dk) / dk
-        for ipb in range(npb):
-            ldm[ipb, :] = (1.0 - ak) * dot(weights[ik], ldp[0, ipb, :]) + ak * dot(weights[ik + 1], ldp[0, ipb, :])
-    else:
-        _, _, wg = calculate_weights_2d(kmean, z_edges, ng)
-        for ipb in range(npb):
-            ldm[ipb, :] = dot(wg, ldp[0, ipb, :])
-
-    # -----------------------------------------------------#
-    # Calculate the Taylor series expansions for the orbit #
-    # -----------------------------------------------------#
-    xyc[:, :] = solve_xy_p5s(0.0, p, a, i, e, w)
-
-    # --------------------------------#
-    # Calculate the half-window width #
-    # --------------------------------#
-    hww = 0.5 * d_from_pkaiews(p, kmean, a, i, e, w, 1, 14)
-    hww = 0.0015 + exptimes + hww
-
-    # --------------------------#
-    # Calculate the light curve #
-    # --------------------------#
-    flux = zeros((npb, npt))
-    for ipt in range(npt):
-        epoch = floor((times[ipt] - t0 + 0.5 * p) / p)
-        tc = times[ipt] - (t0 + epoch * p)
-        if fabs(tc) > hww:
-            flux[:, ipt] = 1.0
+        # -----------------------------------#
+        # Calculate the limb darkening means #
+        # -----------------------------------#
+        if kmin <= kmean <= kmax:
+            ik = int(floor((kmean - kmin) / dk))
+            ak = (kmean - kmin - ik * dk) / dk
+            for ipb in range(npb):
+                ldm[ipb, :] = (1.0 - ak) * dot(weights[ik], ldp[ipv, ipb, :]) + ak * dot(weights[ik + 1],
+                                                                                         ldp[ipv, ipb, :])
         else:
-            for isample in range(1, nsamples + 1):
-                time_offset = exptimes * ((isample - 0.5) / nsamples - 0.5)
-                z = pd_t15sc(tc + time_offset, xyc)
-                aplanet = ccia(1.0, kmean, z)[0]
-                for ipb in range(npb):
-                    iplanet = interpolate_mean_limb_darkening_s(z / (1.0 + kmean), dg, ldm[ipb])
-                    flux[ipb, ipt] += (istar[0, ipb] - iplanet * aplanet * afac[ipb]) / istar[0, ipb]
-            flux[:, ipt] /= nsamples
+            _, _, wg = calculate_weights_2d(kmean, z_edges, ng)
+            for ipb in range(npb):
+                ldm[ipb, :] = dot(wg, ldp[ipv, ipb, :])
+
+        # -----------------------------------------------------#
+        # Calculate the Taylor series expansions for the orbit #
+        # -----------------------------------------------------#
+        xyc[:, :] = solve_xy_p5s(0.0, p[ipv], a[ipv], i[ipv], e[ipv], w[ipv])
+
+        # --------------------------------#
+        # Calculate the half-window width #
+        # --------------------------------#
+        hww = 0.5 * d_from_pkaiews(p[ipv], kmean, a[ipv], i[ipv], e[ipv], w[ipv], 1, 14)
+        hww = 0.0015 + exptimes + hww
+
+        # --------------------------#
+        # Calculate the light curve #
+        # --------------------------#
+        for ipt in range(npt):
+            epoch = floor((times[ipt] - t0 + 0.5 * p[ipv]) / p[ipv])
+            tc = times[ipt] - (t0 + epoch * p[ipv])
+            if fabs(tc) > hww:
+                flux[ipv, :, ipt] = 1.0
+            else:
+                for isample in range(1, nsamples + 1):
+                    time_offset = exptimes * ((isample - 0.5) / nsamples - 0.5)
+                    z = pd_t15sc(tc[ipv] + time_offset, xyc)
+                    aplanet = ccia(1.0, kmean, z)[0]
+                    for ipb in range(npb):
+                        iplanet = interpolate_mean_limb_darkening_s(z / (1.0 + kmean), dg, ldm[ipb])
+                        flux[ipv, ipb, ipt] += (istar[ipv, ipb] - iplanet * aplanet * afac[ipb]) / istar[ipv, ipb]
+                flux[ipv, :, ipt] /= nsamples
     return flux
