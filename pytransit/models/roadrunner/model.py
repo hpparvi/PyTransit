e336161453fa351eaa8d5f09d0ceb3d060668edf
Hannu Parviainen <hpparvi@gmail.com>
2024-02-02 21:21:03 +0000

    RRModel bug fixes.

--- pytransit/models/roadrunner/model.py
+++ pytransit/models/roadrunner/model.py
@@ -9,12 +9,13 @@ def rrmodel(times, k, t0, p, a, i, e, w,
             lcids, pbids, epids, nsamples, exptimes,
             ldp, istar, weights, dk, kmin, kmax, dg, z_edges):
 
-    if npb > 1 or nep > 1 or isinstance(k, ndarray):
-        k, t0, p, a, i, e, w = atleast_2d(k), atleast_2d(t0), atleast_1d(p), atleast_1d(a), atleast_1d(i), atleast_1d(e), atleast_1d(w)
+    k, t0, p, a, i, e, w = (atleast_2d(k), atleast_2d(t0), atleast_1d(p), atleast_1d(a),
+                            atleast_1d(i), atleast_1d(e), atleast_1d(w))
+
+    if nlc > 1:
         return squeeze(rr_full(times, k, t0, p, a, i, e, w, parallelize, nlc, npb, nep,
                        lcids, pbids, epids, nsamples, exptimes,
                        ldp, istar, weights, dk, kmin, kmax, dg, z_edges))
     else:
-        return rr_simple(times, k, t0, p, a, i, e, w, parallelize, nlc, npb, nep,
-                         lcids, pbids, epids, nsamples, exptimes,
-                         ldp, istar, weights, dk, kmin, kmax, dg, z_edges)
+        return rr_simple(times, k[0, 0], t0[0, 0], p[0], a[0], i[0], e[0], w[0], parallelize, nsamples[0], exptimes[0],
+                         ldp[0, 0, :], istar[0, 0], weights, dk, kmin, kmax, dg, z_edges)
--- pytransit/models/roadrunner/model_simple.py
+++ pytransit/models/roadrunner/model_simple.py
@@ -11,38 +11,31 @@ from .common import circle_circle_intersection_area_kite as ccia
 
 @njit
 def rr_simple(times: ndarray, k: float, t0: float, p: float, a: float, i: float, e: float, w: float,
-              parallelize: bool, nlc: int, npb: int, nep: int,
-              lcids: ndarray, pbids: ndarray, epids: ndarray, nsamples: ndarray, exptimes: ndarray,
-              ldp: ndarray, istar: ndarray,
+              parallelize: bool, nsamples: ndarray, exptimes: ndarray, ldp: ndarray, istar: float,
               weights: ndarray, dk: float, kmin: float, kmax: float, dg: float, z_edges: ndarray):
     """Simplified RoadRunner model for a single homogeneous light curve."""
 
     if parallelize:
-        return rr_simple_parallel(times, k, t0, p, a, i, e, w, nlc, npb, nep,
-                                  lcids, pbids, epids, nsamples, exptimes,
-                                  ldp, istar, weights, dk, kmin, kmax, dg, z_edges)
+        return rr_simple_parallel(times, k, t0, p, a, i, e, w, nsamples, exptimes,
+                                ldp, istar, weights, dk, kmin, kmax, dg, z_edges)
     else:
-        return rr_simple_serial(times, k, t0, p, a, i, e, w, nlc, npb, nep,
-                                lcids, pbids, epids, nsamples, exptimes,
+        return rr_simple_serial(times, k, t0, p, a, i, e, w, nsamples, exptimes,
                                 ldp, istar, weights, dk, kmin, kmax, dg, z_edges)
 
 
 @njit(parallel=False, fastmath=False)
 def rr_simple_serial(times: ndarray, k: float, t0: float, p: float, a: float, i: float, e: float, w: float,
-                     nlc: int, npb: int, nep: int,
-                     lcids: ndarray, pbids: ndarray, epids: ndarray, nsamples: ndarray, exptimes: ndarray,
-                     ldp: ndarray, istar: ndarray,
+                     nsamples: ndarray, exptimes: ndarray, ldp: ndarray, istar: float,
                      weights: ndarray, dk: float, kmin: float, kmax: float, dg: float, z_edges: ndarray):
     """Simplified RoadRunner model for a single homogeneous light curve."""
 
     npt = times.size
     ng = weights.shape[1]
-    _istar = istar[0, 0]
 
     ldm = zeros(ng)  # Limb darkening means
     xyc = zeros((2, 5))  # Taylor series coefficients for the (x, y) position
 
-    if isnan(a) or (a <= 1.0) or (e < 0.0) or (isnan(ldp[0, 0])):
+    if isnan(a) or (a <= 1.0) or (e < 0.0) or (isnan(ldp[0])):
         return full(npt, nan)
 
     # ----------------------------------#
@@ -51,10 +44,10 @@ def rr_simple_serial(times: ndarray, k: float, t0: float, p: float, a: float, i:
     if kmin <= k <= kmax:
         ik = int(floor((k - kmin) / dk))
         ak = (k - kmin - ik * dk) / dk
-        ldm[:] = (1.0 - ak) * dot(weights[ik], ldp[0, 0]) + ak * dot(weights[ik + 1], ldp[0, 0])
+        ldm[:] = (1.0 - ak) * dot(weights[ik], ldp) + ak * dot(weights[ik + 1], ldp)
     else:
         _, _, wg = calculate_weights_2d(k, z_edges, ng)
-        ldm[:] = dot(wg, ldp[0, 0])
+        ldm[:] = dot(wg, ldp)
 
     # -----------------------------------------------------#
     # Calculate the Taylor series expansions for the orbit #
@@ -71,38 +64,35 @@ def rr_simple_serial(times: ndarray, k: float, t0: float, p: float, a: float, i:
     # Calculate the light curve #
     # --------------------------#
     flux = zeros(npt)
-    for ipt in prange(npt):
+    for ipt in range(npt):
         epoch = floor((times[ipt] - t0 + 0.5 * p) / p)
         tc = times[ipt] - (t0 + epoch * p)
         if fabs(tc) > hww:
             flux[ipt] = 1.0
         else:
-            for isample in range(1, nsamples + 1):
+            for isample in range(1, nsamples+ 1):
                 time_offset = exptimes * ((isample - 0.5) / nsamples - 0.5)
                 z = pd_t15sc(tc + time_offset, xyc)
                 iplanet = interpolate_mean_limb_darkening_s(z / (1.0 + k), dg, ldm)
                 aplanet = ccia(1.0, k, z)[0]
-                flux[ipt] += (_istar - iplanet * aplanet) / _istar
+                flux[ipt] += (istar - iplanet * aplanet) / istar
             flux[ipt] /= nsamples
     return flux
 
 
 @njit(parallel=True, fastmath=False)
 def rr_simple_parallel(times: ndarray, k: float, t0: float, p: float, a: float, i: float, e: float, w: float,
-                       nlc: int, npb: int, nep: int,
-                       lcids: ndarray, pbids: ndarray, epids: ndarray, nsamples: ndarray, exptimes: ndarray,
-                       ldp: ndarray, istar: ndarray,
-                       weights: ndarray, dk: float, kmin: float, kmax: float, dg: float, z_edges: ndarray):
+                     nsamples: ndarray, exptimes: ndarray, ldp: ndarray, istar: float,
+                     weights: ndarray, dk: float, kmin: float, kmax: float, dg: float, z_edges: ndarray):
     """Simplified RoadRunner model for a single homogeneous light curve."""
 
     npt = times.size
     ng = weights.shape[1]
-    _istar = istar[0, 0]
 
     ldm = zeros(ng)  # Limb darkening means
     xyc = zeros((2, 5))  # Taylor series coefficients for the (x, y) position
 
-    if isnan(a) or (a <= 1.0) or (e < 0.0) or (isnan(ldp[0, 0])):
+    if isnan(a) or (a <= 1.0) or (e < 0.0) or (isnan(ldp[0])):
         return full(npt, nan)
 
     # ----------------------------------#
@@ -111,10 +101,10 @@ def rr_simple_parallel(times: ndarray, k: float, t0: float, p: float, a: float, 
     if kmin <= k <= kmax:
         ik = int(floor((k - kmin) / dk))
         ak = (k - kmin - ik * dk) / dk
-        ldm[:] = (1.0 - ak) * dot(weights[ik], ldp[0, 0]) + ak * dot(weights[ik + 1], ldp[0, 0])
+        ldm[:] = (1.0 - ak) * dot(weights[ik], ldp) + ak * dot(weights[ik + 1], ldp)
     else:
         _, _, wg = calculate_weights_2d(k, z_edges, ng)
-        ldm[:] = dot(wg, ldp[0, 0])
+        ldm[:] = dot(wg, ldp)
 
     # -----------------------------------------------------#
     # Calculate the Taylor series expansions for the orbit #
@@ -137,11 +127,12 @@ def rr_simple_parallel(times: ndarray, k: float, t0: float, p: float, a: float, 
         if fabs(tc) > hww:
             flux[ipt] = 1.0
         else:
-            for isample in range(1, nsamples + 1):
+            for isample in range(1, nsamples+ 1):
                 time_offset = exptimes * ((isample - 0.5) / nsamples - 0.5)
                 z = pd_t15sc(tc + time_offset, xyc)
                 iplanet = interpolate_mean_limb_darkening_s(z / (1.0 + k), dg, ldm)
                 aplanet = ccia(1.0, k, z)[0]
-                flux[ipt] += (_istar - iplanet * aplanet) / _istar
+                flux[ipt] += (istar - iplanet * aplanet) / istar
             flux[ipt] /= nsamples
     return flux
+
