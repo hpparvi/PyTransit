31976334bb409c5c299c0ed07ad031f5f313998e
Hannu Parviainen <hpparvi@gmail.com>
2024-10-08 20:14:07 +0100

    Improved the eclipse-circle intersection routines.

--- pytransit/models/roadrunner/ecintersection.py
+++ pytransit/models/roadrunner/ecintersection.py
@@ -4,9 +4,93 @@ from numpy import ndarray, fabs, pi, full, arange
 from numba import njit
 
 @njit
-def create_ellipse(ny: int, k: float, f: float, a: float) -> (ndarray, ndarray):
+def rotated_ellipse_bbox(a: float, f: float):
     """
-    Create the coordinates for a rotated ellipse.
+    Parameters
+    ----------
+    a : float
+        The angle (in radians) by which the ellipse is rotated.
+    f : float
+        The eccentricity of the ellipse, with a value between 0 and 1.
+
+    Returns
+    -------
+    hw : float
+        The half-width of the bounding box of the rotated ellipse.
+    hh : float
+        The half-height of the bounding box of the rotated ellipse.
+
+    """
+    r1, r2 = 1.0, 1.0-f
+    ux = r1 * cos(a)
+    uy = r1 * sin(a)
+    vx = r2 * cos(a + pi/2)
+    vy = r2 * sin(a + pi/2)
+    hw = sqrt(ux*ux + vx*vx)
+    hh = sqrt(uy*uy + vy*vy)
+    return hw, hh
+
+@njit
+def rotated_ellipse_x(y: float, a: float, f: float) -> tuple[float, float]:
+    """Calculate the x coordinates for a rotated ellipse given the y coordinate and the flattening factor.
+
+    Parameters
+    ----------
+    y
+        The y-coordinate of the point on the ellipse.
+    a
+        The angle (in radians) of rotation for the ellipse.
+    f
+        The flattening parameter of the ellipse.
+
+    Returns
+    -------
+    tuple[float, float]
+        The ellipse x-coordinates for the given y coordinate.
+    """
+    ca, sa = cos(a), sin(a)
+    b = 1.0 - f
+    d = b**2*ca**2 - y**2*sa**4 - 2*y**2*sa**2*ca**2 - y**2*ca**4 + sa**2
+    if d >= 0.0:
+        xl = (y*(-sa*ca + sa*ca/b**2) - sqrt(d)/b) / (ca**2 + sa**2/b**2)
+        xr = (y*(-sa*ca + sa*ca/b**2) + sqrt(d)/b) / (ca**2 + sa**2/b**2)
+        return xl, xr
+    else:
+        return nan, nan
+
+
+@njit
+def rotated_ellipse_dxdy(y: float, a: float, f: float) -> tuple[float, float]:
+    """Calculate dx/dy for a rotated ellipse given the y coordinate and the flattening factor.
+
+    Parameters
+    ----------
+    y
+        The y-coordinate of the point on the ellipse.
+    a
+        The angle, in radians, of rotation of the ellipse.
+    f
+        The flattening parameter of the ellipse.
+
+    Returns
+    -------
+    tuple[float, float]
+        The ellipse derivatives (dx/dy) for the given y coordinate.
+    """
+    ca, sa = cos(a), sin(a)
+    b = 1.0 - f
+    d = b**2*ca**2 - y**2*sa**4 - 2*y**2*sa**2*ca**2 - y**2*ca**4 + sa**2
+    if d >= 0.0:
+        dxl = (-sa*ca - (-y*sa**4 - 2*y*sa**2*ca**2 - y*ca**4)/(b*sqrt(d)) + sa*ca/b**2)/(ca**2 + sa**2/b**2)
+        dxr = (-sa*ca + (-y*sa**4 - 2*y*sa**2*ca**2 - y*ca**4)/(b*sqrt(d)) + sa*ca/b**2)/(ca**2 + sa**2/b**2)
+        return dxl, dxr
+    else:
+        return nan, nan
+
+
+@njit
+def create_ellipse(ny: int, k: float, f: float, a: float) -> (ndarray, ndarray):
+    """Create the coordinates for a rotated ellipse.
 
     Parameters
     ----------
@@ -54,8 +138,7 @@ def create_ellipse(ny: int, k: float, f: float, a: float) -> (ndarray, ndarray):
 
 @njit
 def ellipse_circle_intersection_area(cx: float, cy: float, z: float, k: float, f: float, xs: ndarray, ys: ndarray) -> float:
-    """
-     Calculate the intersection area between a rotated ellipse and a circle using a scanline fill approach.
+    """Calculate the intersection area between a rotated ellipse and a circle using a scanline fill approach.
 
    Parameters
     ----------
