d855e00522c7034de83365b50c3c369b0a64bfe1
Hannu Parviainen <hpparvi@gmail.com>
2026-02-21 22:42:57 +0000

    TransitModel update.

--- pytransit/models/transitmodel.py
+++ pytransit/models/transitmodel.py
@@ -26,66 +26,59 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-from typing import Union, Optional, List
+import jax
+import numba
 
-from numpy import ones, ndarray, asarray, zeros, unique, atleast_1d, issubdtype, integer, float64
+from typing import Literal
 
-from ..orbits.orbits_py import ta_ip_calculate_table
+from numpy import ones, ndarray, asarray, zeros, unique, atleast_1d, issubdtype, integer, float64, array
 
 
 class TransitModel(object):
-    """Exoplanet transit light curve model 
+    """Exoplanet transit light curve model
     """
+    def __init__(self, backend: Literal["numba", "jax"] = "numba", return_grad: bool = False,
+                 parallel: bool = False, n_threads: int | None = None, **kwargs):
+        self.backend = backend
+        self.return_grad = return_grad
+        self.n_threads = n_threads
+        self.parallel = parallel
 
-    def __init__(self) -> None:
-
-        # Declare the basic arrays
-        # ------------------------
-        self.time_id: Optional[int] = None
-        self.time: Optional[ndarray] = None
-        self.lcids: Optional[ndarray] = None
-        self.pbids: Optional[ndarray] = None
-        self.nsamples: Optional[ndarray] = None
-        self.exptimes: Optional[ndarray] = None
-        self.epids: Optional[ndarray] = None
+        self_model = None
+        self.times = None
+        self.lcids = None
+        self.pbids = None
+        self.epids = None
+        self.nsamples = None
+        self.exptimes = None
 
         self.nlc: int = 0
         self.npt: int = 0
         self.npb: int = 0
+        self.nep: int = 0
 
-    def set_data(self, time: Union[ndarray, List],
-                 lcids: Optional[Union[ndarray, List]] = None,
-                 pbids: Optional[Union[ndarray, List]] = None,
-                 nsamples: Optional[Union[ndarray, List]]  = None,
-                 exptimes: Optional[Union[ndarray, List]] = None,
-                 epids: Optional[Union[ndarray, List]] = None) -> None:
-        """Set the data for the transit model.
+        self.simple = True
+        self._time_id: int | None = None
 
-        Parameters
-        ----------
-        time : array-like
-            Array of mid-exposure times for which the model will be evaluated.
-        lcids : array-like, optional
-            Array of integer light curve indices. Must have the same size as the time array.
-        pbids : array-like, optional
-            Array of passband indices, one per light curve. Must satisfy `pbids.size == unique(lcids).size`.
-        nsamples : int or array-like, optional
-            Number of samples per exposure. Can either be an integer, in which case all the light curves will have the
-            same supersampling rate, or an array of integers, in which case each light curve can have a different rate.
-        exptimes : float or array-like, optional
-            Exposure times, again either for all the modelled data, or one value per light curve.
-        epids : array-like, optional
-            Epoch indices that can be used to link a light curve to a specific zero epoch and period (for TTV calculations).
-        """
+        self._init_model()
 
-        # Time samples
-        # ------------
-        if id(time) == self.time_id and lcids is None and pbids is None and nsamples is None and exptimes is None and epids is None:
+    def _init_model(self):
+        raise NotImplementedError
+
+    def set_data(self,
+                 times: ndarray | list,
+                 lcids: ndarray | list | None = None,
+                 pbids: ndarray | list | None = None,
+                 epids: ndarray | list | None = None,
+                 nsamples: ndarray | list | None  = None,
+                 exptimes: ndarray | list | None = None) -> None:
+
+        if id(times) == self._time_id and lcids is None and pbids is None and nsamples is None and exptimes is None and epids is None:
             return
 
-        self.time_id  = id(time)
-        self.time     = asarray(time, float64)
-        self.npt      = self.time.size
+        self.time_id  = id(times)
+        self.times     = array(times, float64)
+        self.npt      = self.times.size
 
         # Light curve indices
         # -------------------
@@ -101,7 +94,6 @@ class TransitModel(object):
 
         # Passband indices
         # ----------------
-        # The passband a light curve belongs to.
         self.pbids    = asarray(pbids) if pbids is not None else zeros(self.nlc, 'int')
         self.npb = unique(self.pbids).size
 
@@ -117,6 +109,13 @@ class TransitModel(object):
         # Epoch indices
         # -------------
         self.epids = asarray(epids) if epids is not None else zeros(self.nlc, 'int')
+        self.nep = unique(self.epids).size
+
+        if self.epids.size != self.nlc:
+            raise ValueError(f"Epoch index array size ({self.epids.size}) should equal to the number of ligt curves ({self.nlc}).")
+
+        if not (self.epids.max() == (self.nep-1) and self.epids.min() == 0):
+            raise ValueError(f"Epoch indices (`epids`) for {self.nep} unique epochs should be given as integers between 0 and {self.nep - 1}.")
 
         # Supersampling
         # -------------
@@ -124,12 +123,30 @@ class TransitModel(object):
         self.nsamples = atleast_1d(nsamples) if nsamples is not None else ones(self.nlc, 'int')
         self.exptimes = atleast_1d(exptimes) if exptimes is not None else zeros(self.nlc, 'int')
 
-    def __call__(self, *nargs, **kwargs):
+        if self.npb > 1 or self.nep > 1:
+            self.simple = False
+
+        if self.backend == "jax":
+            self.times = jax.device_put(self.times)
+            self.lcids = jax.device_put(self.lcids)
+            self.epids = jax.device_put(self.epids)
+            self.nsamples = jax.device_put(self.nsamples)
+            self.exptimes = jax.device_put(self.exptimes)
+
+    def evaluate(self,
+                 k: float | ndarray,
+                 t0: float | ndarray,
+                 p: float | ndarray,
+                 a: float | ndarray,
+                 i: float | ndarray,
+                 e: float | ndarray = 0.0,
+                 w: float | ndarray = 0.0,
+                 d: float | ndarray | None = None,
+                 b: float | ndarray | None = None,
+                 g: float | ndarray | None = None,
+                 rho: float | ndarray | None = None,
+                 secw: float | ndarray | None = None,
+                 sesw: float | ndarray | None = None,
+                 **kwargs) -> ndarray:
         raise NotImplementedError
 
-    # Default evaluation methods
-    # --------------------------
-    def evaluate(self, k: Union[float, ndarray], ldc: ndarray, t0: Union[float, ndarray], p: Union[float, ndarray],
-                 a: Union[float, ndarray], i: Union[float, ndarray], e: Union[float, ndarray] = None, w: Union[float, ndarray] = None,
-                 copy: bool = True) -> ndarray:
-        raise NotImplementedError
