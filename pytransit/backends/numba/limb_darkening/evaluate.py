8df7326683dbc1df1a6ac564082979a3a09bd77c
Hannu Parviainen <hpparvi@gmail.com>
2026-02-21 22:43:36 +0000

    Added limb darkening model evaluation routines.

---
+++ pytransit/backends/numba/limb_darkening/evaluate.py
@@ -0,0 +1,38 @@
+from numba import njit
+from numpy import zeros
+
+
+@njit
+def evaluate_ld(ldm, mu, pvo):
+    if pvo.ndim == 1:
+        pv = pvo.reshape((1, 1, -1))
+    elif pvo.ndim == 2:
+        pv = pvo.reshape((1, pvo.shape[1], -1))
+    else:
+        pv = pvo
+
+    npv = pv.shape[0]
+    npb = pv.shape[1]
+    ldp = zeros((npv, npb, mu.size))
+    for ipv in range(npv):
+        for ipb in range(npb):
+            ldp[ipv, ipb, :] = ldm(mu, pv[ipv, ipb])
+    return ldp
+
+
+@njit
+def evaluate_ldi(ldi, pvo):
+    if pvo.ndim == 1:
+        pv = pvo.reshape((1, 1, -1))
+    elif pvo.ndim == 2:
+        pv = pvo.reshape((1, pvo.shape[1], -1))
+    else:
+        pv = pvo
+
+    npv = pv.shape[0]
+    npb = pv.shape[1]
+    istar = zeros((npv, npb))
+    for ipv in range(npv):
+        for ipb in range(npb):
+            istar[ipv, ipb] = ldi(pv[ipv, ipb])
+    return istar
