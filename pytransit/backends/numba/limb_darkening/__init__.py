ef9e9c46092f474cacb16d8d673570cea8820883
Hannu Parviainen <hpparvi@gmail.com>
2026-02-21 22:43:59 +0000

    Revised inits and requirements.

---
+++ pytransit/backends/numba/limb_darkening/__init__.py
@@ -0,0 +1,8 @@
+from .general import ld_general, ldd_general, ldi_general
+from .linear import ld_linear, ldd_linear, ldi_linear
+from .nonlinear import ld_nonlinear, ldd_nonlinear, ldi_nonlinear
+from .power_2 import ld_power_2, ldd_power_2, ldi_power_2
+from .quadratic import ld_quadratic, ldd_quadratic, ldi_quadratic
+from .quadratic_tri import ld_quadratic_tri, ldd_quadratic_tri, ldi_quadratic_tri
+from .uniform import ld_uniform, ldd_uniform, ldi_uniform
+from .evaluate import evaluate_ld, evaluate_ldi
