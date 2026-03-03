c8654265b0528a5a6f98b4630e0f15cf6c90e736
Hannu Parviainen <hannu@iac.es>
2026-03-03 15:19:02 +0100

    Added `udmodel_full` method to compute flux for multiple parameter vectors and epochs. Imported new utilities and adjusted bounding box logic.

--- pytransit/backends/numba/udmodel.py
+++ pytransit/backends/numba/udmodel.py
@@ -1,7 +1,9 @@
 from meepmeep.backends.numba.ts2d import pd_t15c, solve_xy_p5
+from meepmeep.backends.numba.ts2d.position import bounding_box
 from meepmeep.backends.numba.utils import d_from_pkaiews
+
 from numba import njit, prange
-from numpy import floor, pi, zeros, nan, fabs
+from numpy import floor, pi, zeros, nan, fabs, unique, isnan
 from .ccintersection import ccia
 
 
@@ -97,4 +99,40 @@ def udmodel(times, k, t0, p, a, i, e, w):
         t = _folded_time(times[j], t0, p)
         if fabs(t) < half_window_width:
             _udmodel(t, k, cf, flux[j:j + 1])
+    return flux
+
+
+def udmodel_full(times, k, t0, p, a, i, e, w, lcids, pbids, epids, nsamples, exptimes):
+
+    npt = times.size   # Number of points
+    npv = k.shape[0]   # Number of parameter vectors
+    npb = k.shape[1]   # Number of passbands
+    nep = t0.shape[1]   # Number of epochs
+    flux = zeros((npv, npt))
+
+    for ipv in range(npv):
+
+        xyc = zeros((nep, 2, 5))
+        xyc[0, :, :] = solve_xy_p5(0.0, p[ipv, 0], a[ipv, 0], i[ipv, 0], e[ipv, 0], w[ipv, 0])
+        if nep > 1 and isnan(p[ipv, 1]):
+            xyc[:, :, :] = xyc[0:1, :, :]
+        else:
+            for iep in range(nep):
+                xyc[iep, :, :] = solve_xy_p5(0.0, p[ipv, iep], a[ipv, iep], i[ipv, iep], e[ipv, iep], w[ipv, iep])
+
+        bt1, bt4 = bounding_box(k[ipv, 0], xyc[0])
+        bt1 -= 0.003 + exptimes[0]
+        bt4 += 0.003 + exptimes[0]
+
+        for ipt in range(npt):
+            ilc = lcids[ipt]
+            ipb = pbids[ilc]
+            iep = epids[ilc]
+
+            t = _folded_time(times[ipt], t0[ipv, iep], p[ipv, iep])
+            if (bt1 <= t <= bt4):
+                for isample in range(1, nsamples[ilc] + 1):
+                    time_offset = exptimes[ilc] * ((isample - 0.5) / nsamples[ilc] - 0.5)
+                    _udmodel(t + time_offset, k[ipv, ipb], xyc[iep], flux[ipv:ipv+1, ipt:ipt+1])
+                flux[ipv, ipt] /= nsamples[ilc]
     return flux
