1bdffc8ce0800dc1d0ab07dea41eea06f560d4c4
Hannu Parviainen <hannu@iac.es>
2026-03-08 12:52:11 +0100

    Added an additive jitter-term to WNLogLikelihood.

--- pytransit/loglikelihoods/wnloglikelihood.py
+++ pytransit/loglikelihoods/wnloglikelihood.py
@@ -12,12 +12,13 @@ class WNLogLikelihood(LogLikelihood):
 
         if self.tm.backend == 'jax':
             model = self.tm.get_callable()
-            def loglike(k, t0, p, a, i, e, w):
+            def loglike(k, t0, p, a, i, e, w, jitter):
                 fmod = model(k, t0, p, a, i, e, w)
-                return - 0.5*self.obs.size*jnp.log(2*jnp.pi) - 0.5*jnp.sum(jnp.log(self.err**2)) - 0.5*jnp.sum((self.obs - fmod)**2 / self.err**2)
+                e2 = self.err**2 + jitter**2
+                return - 0.5*self.obs.size*jnp.log(2*jnp.pi) - 0.5*jnp.sum(jnp.log(e2)) - 0.5*jnp.sum((self.obs - fmod)**2 / e2)
 
             if self.tm.return_grad:
-                self._loglikelihood = jax.jit(jax.value_and_grad(loglike, argnums=tuple(range(7))))
+                self._loglikelihood = jax.jit(jax.value_and_grad(loglike, argnums=tuple(range(8))))
             else:
                 self._loglikelihood = jax.jit(loglike)
 
