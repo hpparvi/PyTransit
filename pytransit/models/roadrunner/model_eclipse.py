91a9b48221df64bcf5f37fcc95aa8311a7224859
Hannu Parviainen <hpparvi@gmail.com>
2026-05-07 15:34:32 +0100

    Continued working on a new multiplicative secondary eclipse model.

--- pytransit/models/roadrunner/model_eclipse.py
+++ pytransit/models/roadrunner/model_eclipse.py
@@ -3,15 +3,17 @@ from meepmeep.tsorbit import bounding_box
 from meepmeep.utils import eclipse_phase
 
 from numba import njit, prange
-from numpy import zeros, dot, ndarray, isnan, nan, full, floor, pi
+from numpy import zeros, isnan, nan, full, floor, pi
+from numpy.typing import NDArray
 
 from meepmeep.xy.position import solve_xy_p5s, pd_t15sc
 
 from .common import circle_circle_intersection_area_kite as ccia
 
-def eclipse_model(times: ndarray, k: ndarray, t0: ndarray, p: ndarray, a: ndarray, i: ndarray, e: ndarray, w: ndarray,
-                   rstar: ndarray, nlc: int, npb: int, nep: int,
-                   lcids: ndarray, pbids: ndarray, epids: ndarray, nsamples: ndarray, exptimes: ndarray):
+
+def eclipse_model(times: NDArray, k: NDArray, t0: NDArray, p: NDArray, a: NDArray, i: NDArray, e: NDArray, w: NDArray,
+                  rstar: NDArray, nlc: int, lcids: NDArray, epids: NDArray, nsamples: int | NDArray,
+                  exptimes: int | NDArray):
 
     npv = k.shape[0]
     npt = times.size
@@ -25,7 +27,7 @@ def eclipse_model(times: ndarray, k: ndarray, t0: ndarray, p: ndarray, a: ndarra
     _nsamples[:] = nsamples
 
     pv_is_good = full(npv, True)
-    xyc = zeros((npv, 2, 5))     # Taylor series coefficients for the (x, y) position
+    xyc = zeros((npv, 2, 5))  # Taylor series coefficients for the (x, y) position
     bbs = zeros((npv, nlc, 2))
     eclipse_shifts = zeros(npv)
     ltts = zeros(npv)
@@ -45,7 +47,7 @@ def eclipse_model(times: ndarray, k: ndarray, t0: ndarray, p: ndarray, a: ndarra
         # -----------------------------#
         # Calculate the bounding boxes #
         # -----------------------------#
-        bt1, bt4 = bounding_box(k[ipv], xyc)
+        bt1, bt4 = bounding_box(k[ipv], xyc[ipv])
         bbs[ipv, :, 0] = bt1
         bbs[ipv, :, 1] = bt4
         for ilc in range(nlc):
@@ -67,15 +69,15 @@ def eclipse_model(times: ndarray, k: ndarray, t0: ndarray, p: ndarray, a: ndarra
         ilc = lcids[ipt]
         iep = epids[ilc]
 
-        te =  t0[ipv, iep] + eclipse_shifts[ipv] + ltts[ipv]
+        te = t0[ipv, iep] + eclipse_shifts[ipv] + ltts[ipv]
         epoch = floor((times[ipt] - te + 0.5 * p[ipv]) / p[ipv])
         tc = times[ipt] - (te + epoch * p[ipv])
         if not (bbs[ipv, ilc, 0] <= tc <= bbs[ipv, ilc, 1]):
-            flux[ipv, ipt] = 1.0
+            flux[ipv, ipt] = pi * k[ipv] ** 2
         else:
             for isample in range(1, nsamples[ilc] + 1):
                 time_offset = exptimes[ilc] * ((isample - 0.5) / nsamples[ilc] - 0.5)
                 z = pd_t15sc(tc + time_offset, xyc[ipv])
-                flux[ipv, ipt] +=  1.0 - ccia(1.0, k[ipv], z)[0] / pi
+                flux[ipv, ipt] += pi * k[ipv] ** 2 - ccia(1.0, k[ipv], z)[0]
             flux[ipv, ipt] /= nsamples[ilc]
     return flux
